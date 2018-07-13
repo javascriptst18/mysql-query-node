20b2ecc7d7f0d671ddae19bab1c38822dcdeeb2b
Denizhan Kilic <denizhankilic@gmail.com>
2018-07-13 10:29:04 +0200

    Added a query file that is read if there is no terminal parameter

--- sql-query.js
+++ sql-query.js
@@ -5,12 +5,7 @@ const fs = require('fs');
 const mysql = require('mysql');
 
 // Define database objects with login information for different databases
-const db4free = {
-  host: '85.10.205.173',
-  user: 'academy_db_user',
-  password: 'academy18',
-  database: 'academy_db',
-};
+
 
 const localhost = {
   host: 'localhost',
@@ -19,8 +14,9 @@ const localhost = {
   database: 'test_db',
 };
 
-// Assign the log file path to a variable
+// Assign paths to the log and query files
 const logFile = './log.txt';
+const queryStringFile = './query.sql'
 
 // Select the database to use
 const db = localhost;
@@ -28,8 +24,21 @@ const db = localhost;
 // Pass the selected object to the mysql driver
 const connection = mysql.createConnection(db);
 
-// "process.argv[2]" is the first argument you pass to the node app when running it from the terminal. i.e. "node sql-query <ARGUMENT>"
-const queryString = process.argv[2];
+// Define a variable to store the query string
+let queryString = '';
+
+/** 
+  *  "process.argv[2]" is the first argument you pass to 
+  *   the node app when running it from the terminal. i.e.
+  *  "node sql-query <ARGUMENT>"
+  */
+
+ if (process.argv[2]) { // If an argument is passed in via terminal, use that
+  queryString = process.argv[2];
+} else { // If no argument is passed in via terminal, get the query string from a file instead
+  queryString = fs.readFileSync(queryStringFile,'utf8');
+  console.log(queryString);
+}
 
 // Log the connection attempt to a log file
 fs.appendFileSync(logFile, `\n${new Date().toGMTString()}\nConnecting to ${db.host} as ${db.user}\nQuery sent: "${queryString}"\n`);
@@ -37,18 +46,23 @@ fs.appendFileSync(logFile, `\n${new Date().toGMTString()}\nConnecting to ${db.ho
 // Open a connection to the selected database
 connection.connect();
 
-// Send the queryString (which is <ARGUMENT> from when you ran the app in the terminal with "node sql-query <ARGUMENT>")
+/**   Send the queryString, which either <ARGUMENT> from when you 
+  *   ran the app in the terminal with "node sql-query <ARGUMENT>"
+  *   or the string in query string file if there is no <ARGUMENT>
+  *   passed via terminal.
+  */
 connection.query(queryString, (error, results, fields) => {
-  if (error) {
+  if (error) { // If there's an error, log it
     console.log(error);
     fs.appendFileSync(logFile, `Query failed: ${error}\n`);
-  } else {
+  } else if (Array.isArray(results)) { // If not publish results
     fs.appendFileSync(logFile, 'Query successful. Showing results:\n\n-- Start of results --\n\n');
     results.map((resultArrayElement) => {
       console.log(resultArrayElement);
       fs.appendFileSync(logFile, `${JSON.stringify(resultArrayElement)}\n`);
     })
     fs.appendFileSync(logFile, `\n-- End of results --\n`);
+  } else {
+    console.log(results);
   }
 });
-
